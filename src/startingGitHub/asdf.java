c8286f95032e7f0d8df41c8863df8d6666465fc8
Rajesh <Rajesh@IBM927-PC06XK08>
2018-05-16 00:28:25 +0530

    update

--- src/startingGitHub/asdf.java
+++ src/startingGitHub/asdf.java
@@ -1,9 +1,19 @@
 package startingGitHub;
 
+import java.util.Date;
+
 public class asdf {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello From India");	
+		System.out.println("Time :"+new Date().toInstant());
 		
-		System.out.println("Hello From India");
+		/*Git hub Url
+		https://github.com/rajesh7891/learning.git
+
+		rajesh7891
+		prasidH@11*/
+		
 		
 	}
 
