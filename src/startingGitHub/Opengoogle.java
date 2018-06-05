edd1483c458a104b3212e26053be6d729269545f
Rajesh <Rajesh@IBM927-PC06XK08.in.ibm.com>
2018-06-05 16:10:42 +0530

    updated driver

--- src/startingGitHub/Opengoogle.java
+++ src/startingGitHub/Opengoogle.java
@@ -4,7 +4,7 @@ package startingGitHub;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Opengoogle {
 	
@@ -12,8 +12,8 @@ public class Opengoogle {
 	public void githubint(){
 		System.out.println("Integration Print");
 		
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\IBM_ADMIN\\Desktop\\Learning\\Selenium\\Drivers\\geckodriver.exe");		
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\IBM_ADMIN\\Desktop\\Learning\\Selenium\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		System.out.println(driver.getTitle());
 		Assert.assertTrue("Not same as title", driver.getTitle().contains("Google"));		
